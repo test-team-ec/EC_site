dd8bb2d6723eadf21d1e9410c570e8fad1f01690
Ono <soichrooono@gmail.com>
2020-06-17 06:02:21 +0000

    [Add]customer controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,13 @@
+class CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/update'
+  get 'customers/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
