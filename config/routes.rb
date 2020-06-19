22cffd1837d9c2f1a3e53bc1bf525bd9f39af3e6
vagrant <vagrant@localhost.localdomain>
2020-06-19 03:20:51 +0000

    customer edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'cart_items/confirm' =>"cart_items#confirm"
