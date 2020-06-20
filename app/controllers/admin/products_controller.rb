64dbae4df2b4f4706d0e6399cc049d2dfab2abba
vagrant <vagrant@localhost.localdomain>
2020-06-20 06:56:13 +0000

    admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -12,8 +12,16 @@ class Admin::ProductsController < ApplicationController
 	end
 
 	def new
+		@product = Product.new
 	end
 
 	def create
+		@product = Product.new
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :introduction, :price)
 	end
 end
