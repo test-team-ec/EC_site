69e5e31239a288a37180a5e73e9f38a5275a1e78
vagrant <vagrant@localhost.localdomain>
2020-06-21 03:46:21 +0000

    po

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,26 +8,26 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @order_adress = OrderAress.new(order_adress_params)
+    @order_address = OrderAddress.new(order_address_params)
   end
 
   def create
     @order  = current_customer.find(params[:customer_id])
     @order = PaymentMethod.find(params[:payment_method])
-    @adrres = adrres_option
+    @addrres = addrres_option
     if adrres_option == 0
       @customer.address
     elsif adrres_option == 1
       @current_customer.shipping.address
     elsif adrres_option == 2
-      @order_adress.new
+      @order_address.new
     end
   end
 
   def confirm
     @order  = current_customer.order.find(params[:customer_id])
     @payment = PaymentMethod.find(params[:payment_method])
-    @address = AdrresOption.find(params[:adress])
+    @address = AddrresOption.find(params[:address])
     @order.save
     redirect_to root_path
   end
@@ -39,8 +39,8 @@ class OrdersController < ApplicationController
 
   private
 
-    def order_adress_params
-     @order_adress = OrderAress.new(params.require(:order).permit(:adrres, :postcode, :payment_method, :name))
+    def order_address_params
+     @order_address = OrderAddress.new(params.require(:order).permit(:addrres, :postcode, :payment_method, :name))
     end
 
 
