69ecfb8b587b5bef68a68279248c6277af1c8de8
Ono <soichrooono@gmail.com>
2020-06-21 12:33:55 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  enum address_option: { customer.adress: 0, current_customer.shipping.address: 1, order_adress.new: 2 }
+  enum address_option: { customer.adress: 0, current_customer.shipping.address: 1, order_address.new: 2 }
 end
