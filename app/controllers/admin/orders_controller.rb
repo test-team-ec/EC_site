cd4207c21c714017f49a3f2f7e6970dca91bea5f
Ono <soichrooono@gmail.com>
2020-06-22 11:27:32 +0000

    [Add]admin_order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,5 +11,20 @@ class Admin::OrdersController < ApplicationController
 	end
 
 	def update
+		@order = Order.find(params[:id])
+		@order_details = OrderDetail.where(params[:order_id])
+		@order.update(order_params)
+		if @order.order_status == "入金確認"
+			@order_details.each do |order_detail|
+				order_detail.update(work_status: "制作待ち")
+			end
+		end
+		redirect_to admin_order_path(@order)
 	end
+
+	private
+	def order_params
+		params.require(:order).permit(:order_status)
+	end
+
 end
