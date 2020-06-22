2a707e29c11b93fab5a10177ff144a677b1991bf
Ono <soichrooono@gmail.com>
2020-06-22 02:49:17 +0000

    [Fix]devise_administer [Add]admin_order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,13 @@
 class Admin::OrdersController < ApplicationController
 	def index
+		@orders = Order.all.order(id:"DESC")
+		@order_details = OrderDetail.where(params[:order_id])
+		@total_count = @order_details.all.sum(:count)
 	end
 
 	def show
+		@order = Order.find(params[:id])
+		@order_details = OrderDetail.where(params[:order_id])
 	end
 
 	def update
