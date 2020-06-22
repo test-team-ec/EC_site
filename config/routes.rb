9645a10ae0f9a066b56956ba0ab470cfff526419
vagrant <vagrant@localhost.localdomain>
2020-06-22 02:59:37 +0000

    customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,39 @@
 Rails.application.routes.draw do
 
-  devise_for :customers, skip: :all
-　 devise_scope :customers do
-    get 'customers/sign_up' => 'customers/registrations#new'
-    post 'customers/sign_up' => 'customers/registrations#create'
-    delete 'customers/sign_out' => 'customers/sessions#destroy'
-    get 'customers/sign_in' => 'customers/sessions#new'
-    post 'customers/sign_in' => 'customers/sessions#create'
-   end
+#   devise_for :customers, skip: :all
+# devise_scope :customer do
+#     get 'customers/sign_up' => 'customers/registrations#new'
+#     post 'customers/sign_up' => 'customers/registrations#create'
+#     delete 'customers/sign_out' => 'customers/sessions#destroy'
+#     get 'customers/sign_in' => 'customers/sessions#new'
+#     post 'customers/sign_in' => 'customers/sessions#create'
+#    end
 
 
 
 
 
-　devise_for :admins, skip: :all
-  devise_scope :admin do
-    get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
-    post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
-    delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
-  end
+# devise_for :admins, skip: :all
+#   devise_scope :admin do
+#     get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+#     post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+#     delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+#  end
 
+
+
+  devise_for :admins, controllers:{
+      sessions: 'admin/devise/sessions',
+      passwords: 'admin/devise/passwords'
+    }
+
+  devise_for :customers
+
+
+      #sessions: 'public/devise/sessions',
+      #registrations: 'public/devise/registrations'
+      #passwords: 'public/devise/passwwords'
+    #} 
 
 
 
@@ -42,21 +56,17 @@ Rails.application.routes.draw do
       end
     resources :cart_items, only:[:update, :create, :destroy]
   end
-  namespace :admin do
-    resources :customers, only:[:index, :show, :edit, :update]
-  end
 
   resources :products, only:[:index, :show]
-  namespace :admin do
-    resources :products ,only:[:index, :show, :new, :create, :edit, :update]
-  end
+
 
   namespace :admin do
     resources :order_details ,only:[:update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :genres ,only:[:index, :new, :create, :edit, :update]
+    resources :products ,only:[:index, :show, :new, :create, :edit, :update]
   end
 
-  namespace :admin do
-     resources :genres ,only:[:index, :new, :create, :edit, :update]
-  end
+
 
 end
