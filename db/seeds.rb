78e2770c400aa22af15e3b8eafe1ce7a04894a05
Ono <soichrooono@gmail.com>
2020-06-24 07:06:10 +0000

    [Add]header/footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(genre_id: 1, name:"ショートケーキ", price: 200, introduction:"ショートケーキです",
+Product.create(genre_id: 1, name:"チョコケーキ", price: 200, introduction:"チョコケーキです",
 	product_image_id: "", is_active: "販売中")
-Product.create(genre_id: 2, name:"クッキー", price: 100, introduction:"クッキーです",
+Product.create(genre_id: 1, name:"チーズケーキ", price: 200, introduction:"チーズケーキです",
 	product_image_id: "", is_active: "販売中")
-Product.create(genre_id: 3, name:"プリン", price: 100, introduction:"プリンです",
+Product.create(genre_id: 2, name:"チョコクッキー", price: 100, introduction:"チョコクッキーです",
 	product_image_id: "", is_active: "販売中")
-
-Genre.create(name: "ケーキ", is_active:"有効")
-Genre.create(name: "焼き菓子", is_active:"有効")
-Genre.create(name: "プリン", is_active:"無効")
-
-Customer.create(first_name: "test", last_name: "test", first_name_phonetic: "test",
-	last_name_phonetic: "test", tel:"1234567890", postcode: "1234567", address:"東京都",
-	email:"test@test", password:"testtest", is_active:"有効" )
-
-Shipping.create(customer_id: 1, name:"test", postcode:"1234567", address:"大阪府")
-
-Order.create(customer_id: 1, name:"test", postcode: "1234567", address:"東京都",
-	postage: 800, total_products_cost: 330, payment_method:"　クレジットカード", order_status: "入金待ち")
-
-OrderDetail.create(product_id: 1, order_id: 1, count: 1, price: 220, work_status: "着手不可" )
-OrderDetail.create(product_id: 2, order_id: 1, count: 1, price: 110, work_status: "着手不可" )
-
-Administer.create(email: "admin@admin",password: "adminadmin")
